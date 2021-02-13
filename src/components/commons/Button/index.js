db39c88ebb617e30926cb6a7272d3bfaa32d3ba8
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-13 13:53:47 -0300

    applied smallestException typography

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,26 +1,28 @@
 import styled, { css } from 'styled-components';
 import get from 'loadsh/get';
+import { TextStyleVariantMap } from '../../foundation/Text';
 
 const buttonGhost = css`
-  color: ${(({ theme, variant }) => get(theme, `colors.${variant}.color`))};
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
   background: transparent;
-`
+`;
 const buttonDefault = css`
-  color: ${(({ theme, variant }) => get(theme, `colors.${variant}.contrastText`))};
-  background-color: ${(({ theme, variant }) => get(theme, `colors.${variant}.color`))};
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
+  background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
 `;
 
 export const Button = styled.button`
   border: 0;
   cursor: pointer;
   padding: 12px 36px;
-  font-weight: bold;
+  font-weight: 700 !important;
   opacity: 1;
-  ${function (props) {
-    return props.ghost ? buttonGhost : buttonDefault;
-  }}
-  border-radius: ${(({ theme }) => theme.borderRadius)};
-  transition: ${(({ theme }) => theme.transition)};
+  transition: ${({ theme }) => theme.transition};
+  border-radius: ${({ theme }) => theme.borderRadius};
+
+  ${(TextStyleVariantMap.smallestException)}
+
+  ${({ ghost }) => ghost ? buttonGhost : buttonDefault}
   &:hover,
   &:focus {
     opacity: .8
