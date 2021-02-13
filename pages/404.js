c44fe89a042d4c2861fc4e92504633da2b5898f7
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-13 13:52:04 -0300

    file spacing 2 > 4

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 export default function page404() {
-    return (
-        <div>
-            <h1>404</h1>
-            <h2>Ops...</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Ops...</h2>
+    </div>
+  );
 }
