78e5009aed916ee055d66b9ccbe20c5f4e8cd2f2
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-13 22:30:41 -0300

    added main content

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Menu from '../src/components/commons/Menu';
-import Footer from '../src/components/commons/Footer';
+import { Button } from '../src/components/commons/Button';
+import { Menu } from '../src/components/commons/Menu';
+import { Footer } from '../src/components/commons/Footer';
+
+import { Text } from '../src/components/foundation/Text';
 
 export default function Home() {
   return (
@@ -11,6 +14,42 @@ export default function Home() {
       justifyContent: 'space-between',
     }}>
       <Menu />
+
+      <div>
+        <Text
+          variant="title"
+          tag="h1"
+          color="tertiary.main"
+          textAlign={{
+            xs: 'center',
+            md: 'left',
+          }}>
+          Compartilhe momentos e conecte-se com amigos
+          </Text>
+
+        <Text
+          variant="paragraph1"
+          tag="p"
+          color="tertiary.light"
+          textAlign={{
+            xs: 'center',
+            md: 'left',
+          }}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+        </Text>
+
+        <Button
+          variant="primary.main"
+          margin={{
+            xs: 'auto',
+            md: 'initial',
+          }}
+          display='block'>
+          Cadastrar
+          </Button>
+      </div>
+
       <Footer />
     </div>
   );
