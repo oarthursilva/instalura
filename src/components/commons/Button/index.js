a50ddef1b798b76dc012d14fa149ecd086410642
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-04 22:08:11 -0300

    feat(component): add fullWidth and disable properties

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -29,6 +29,15 @@ export const Button = styled.button`
     opacity: .8
   }
 
+  &:disabled {
+    cursor: not-allowed;
+    opacity: .2;
+  }
+
+  ${({ fullWidth }) => fullWidth && css`
+    width: 100%;
+  `}
+
   ${propToStyle('margin')}
   ${propToStyle('display')}
 
