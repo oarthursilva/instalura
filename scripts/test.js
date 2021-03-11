d1c154e64dee7d80aa2249b5872ed69a697f2e91
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-10 23:10:09 -0300

    feat(husky): add husky hook to prevent mistakes during commits

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,6 @@
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 import { exec } from 'shelljs';
 
-const result = exec('git diff --name-only feature/style-form..main', { silent: true });
+const result = exec('git diff --name-only feature/setup-pre-push..main', { silent: true });
 
 console.log(result.stdout.split('\n').filter((data) => data.length));
