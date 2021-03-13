ad513454e5a4377088ee3fcc1ba1a1f2a9fbe952
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-13 18:39:52 -0300

    feat(seo): implement basic feature of SEO at header level

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,21 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
 
+import { SEO } from '../src/components/commons/SEO';
+
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Instalura</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
       </Head>
+      <SEO headerTitle="Home" />
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
