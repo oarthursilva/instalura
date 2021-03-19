698cc744b1f3b4a8fa670a2fcd0b2024c968b6ee
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-18 22:10:07 -0300

    feat(global): add provider and hoc to handle with page components globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
-
-import theme from '../src/theme';
-import GlobalStyle from '../src/theme/GlobalStyle';
+import { Head } from '../src/components/commons/Head';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Head />
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
     </>
   );
 }
---
+++ src/components/wrapper/WebsitePage/hoc/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import WebsiteGlobalProvider from '../provider';
+import WebsitePageWrapper from '..';
+
+export function websitePageHOC(PageComponent, { pageWrapperProps }) {
+  return (props) => (
+    <WebsiteGlobalProvider>
+      <WebsitePageWrapper {...pageWrapperProps}>
+        <PageComponent {...props} />
+      </WebsitePageWrapper>
+    </WebsiteGlobalProvider>
+  );
+}
