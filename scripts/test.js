fbf2cfe4264f94a6f46d2d96eada624221de53f4
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-25 15:58:35 -0300

    feat(scripts): add custom script to run lint at CI phase

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable-next-line import/no-extraneous-dependencies */
-import { exec } from 'shelljs';
+const shelljs = require('shelljs');
 
-const result = exec('git diff --name-only feature/setup-pre-push..main', { silent: true });
+const currentBranch = shelljs.exec('git branch --show-current', { silent: true });
+const result = shelljs.exec(`git diff --name-only ${currentBranch}..main`, { silent: true });
 
 console.log(result.stdout.split('\n').filter((data) => data.length));
