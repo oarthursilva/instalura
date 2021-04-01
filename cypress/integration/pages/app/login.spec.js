501180d3432b3c48f1306c7604326c375f912f47
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-31 23:14:40 -0300

    test(login): add cookie test in integration test of /pages/app/login test

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,16 +2,27 @@
 
 describe('/pages/app/login', () => {
   it('should navigate to profile page when requested form is submitted', () => {
+    cy.intercept('https://instalura-api.vercel.app/api/login')
+      .as('userLogin');
+
     cy.visit('/app/login/');
 
     // fill form
-    cy.get('#formRegister input[name="user"]').type('oarthursilva');
-    cy.get('#formRegister input[name="password"]').type('1234567890');
+    cy.get('#formRegister input[name="username"]').type('oarthursilva');
+    cy.get('#formRegister input[name="password"]').type('senhasegura');
 
     // click on submit
     cy.get('#formRegister button[type="submit"]').click();
 
     // fill form and navigate to profile page at /app/profile
     cy.url().should('include', '/app/profile');
+
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        const { token } = intercept.response.body.data;
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token);
+      });
   });
 });
