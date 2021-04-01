431f4945e158e2e81d4acdb455f161924aeb0934
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-31 23:13:34 -0300

    feat(nookies): use nookies to store the cookie session

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,3 +1,5 @@
+import { destroyCookie, setCookie } from 'nookies';
+
 async function HttpClient(url, { headers, body, ...options }) {
   return fetch(url, {
     headers: {
@@ -24,9 +26,23 @@ export const loginService = {
         password,
       },
     })
-      .then((data) => {
-        console.log(data);
-        return data;
+      .then((response) => {
+        console.log(response);
+
+        const { token } = response.data;
+        const DAY_IN_SECONDS = 86400;
+        setCookie(null, 'APP_TOKEN', token, {
+          path: '/',
+          maxAge: DAY_IN_SECONDS * 7,
+        });
+
+        return {
+          token,
+        };
       });
+  },
+
+  async logout() {
+    destroyCookie(null, 'APP_TOKEN');
   },
 };
