bd65ae6ca0e0571611bfee8ab206a3813b6a6434
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-02 12:41:42 -0300

    test(ut): add test coverage report readable by sonarqube

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ module.exports = {
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules',
     '/node_modules/',
+    'jest.config.js',
+    'test-report.xml',
   ],
   testResultsProcessor: 'jest-sonar-reporter',
   testPathIgnorePatterns: [
