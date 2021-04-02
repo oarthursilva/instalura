c7f9315d6253c2815650744aa4b93d51fbe22b77
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-02 11:48:45 -0300

    test(ut): unit test of propToStyle using multiple breakpoints media

--- src/theme/utils/propToStyle/propToStyle.test.js
+++ src/theme/utils/propToStyle/propToStyle.test.js
@@ -4,7 +4,8 @@ describe('propToStype()', () => {
   describe('when propToStyle receives an sample argument', () => {
     test('and it is a string, should convert to style', () => {
       const propToStyleResult = propToStyle('textAlign');
-      const componentProps = { textAlign: 'center' };
+      // <Text textAlign="center" />
+      const componentProps = { textAlign: 'center' }; // string
       const styleResult = propToStyleResult(componentProps);
 
       expect(styleResult).toEqual({ textAlign: 'center' });
@@ -12,7 +13,8 @@ describe('propToStype()', () => {
 
     test('and it is a number, should convert to style', () => {
       const propToStyleResult = propToStyle('flex');
-      const componentProps = { flex: 1 };
+      // <Box flex={1} />
+      const componentProps = { flex: 1 }; // number
       const styleResult = propToStyleResult(componentProps);
 
       expect(styleResult).toEqual({ flex: 1 });
@@ -20,8 +22,24 @@ describe('propToStype()', () => {
   });
 
   describe('when propToStyle receives an argument with breakpoint', () => {
-    test('', () => {
+    test('render only a single breakpoint resolution', () => {
+      const propToStyleResult = propToStyle('textAlign');
+      // <Text textAlign={{ xs: 'center' }}" />
+      const componentProps = { textAlign: { xs: 'center' } }; // object
+      const styleResult = propToStyleResult(componentProps);
 
+      expect(styleResult).toMatchSnapshot();
+    });
+  });
+
+  describe('when propToStyle receives an argument with breakpoints', () => {
+    test('render multiple breakpoints resolutions', () => {
+      const propToStyleResult = propToStyle('textAlign');
+      // <Text textAlign={{ xs: 'center', md: 'center' }}" />
+      const componentProps = { textAlign: { xs: 'center', md: 'center' } }; // object
+      const styleResult = propToStyleResult(componentProps);
+
+      expect(styleResult).toMatchSnapshot();
     });
   });
 });
