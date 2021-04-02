663c64852bdba0ebcc9b8a38f0bc3f99a84980ac
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-02 10:30:26 -0300

    test(ut): unit test of config/headers

---
+++ config/redirects.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import redirects from './redirects';
+
+console.log(redirects);
+describe('config/redirects', () => {
+  test('when redirect is requrest should match static def', () => {
+    const redirectsExpected = redirects;
+    const redirectsActual = [{
+      source: '/login/',
+      destination: '/app/login/',
+      permanent: true,
+    }];
+    // @Assert
+    expect(redirectsExpected).toEqual(redirectsActual);
+  });
+
+  test('when redirect is requrest should match snapshot', () => {
+    const redirectsExpected = redirects;
+
+    // @Assert
+    expect(redirectsExpected).toMatchSnapshot();
+  });
+});
