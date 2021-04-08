f2fa1c8f602a88f3db689420bb24a24f9666b95b
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-07 23:12:47 -0300

    feat(cms): receive and rendered the about content from Dato CMS

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,5 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
 import AboutScreen from '../../src/components/screen/AboutScreen';
 import { websitePageHOC } from '../../src/components/wrapper/WebsitePage/hoc';
 
@@ -8,3 +10,31 @@ export default websitePageHOC(AboutScreen, {
     },
   },
 });
+
+export async function getStaticProps() {
+  const TOKEN = 'fdd5e9b5aa84cafe513a50facc17f7';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageAbout {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
+}
